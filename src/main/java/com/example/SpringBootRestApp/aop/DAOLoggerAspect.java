15a7d7ecc31a6ac785a4f5abd8daf1968e24a42c
nitesh <nitesh.a125@gmail.com>
2021-12-17 23:12:47 +0530

    Adding logger to DAO class

--- src/main/java/com/example/SpringBootRestApp/aop/DAOLoggerAspect.java
+++ src/main/java/com/example/SpringBootRestApp/aop/DAOLoggerAspect.java
@@ -1,5 +1,6 @@
 package com.example.SpringBootRestApp.aop;
 
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -10,30 +11,30 @@ import java.util.logging.Logger;
 
 @Aspect
 @Component
-public class ControllerLoggerAspect {
+public class DAOLoggerAspect {
 
-    private Logger log = Logger.getLogger(getClass().getName());
+    private Logger log = Logger.getLogger((getClass().getName()));
 
     @Pointcut("execution(* com.example.SpringBootRestApp.controller.*.*(*))")
-    public void controllerPointCut(){}
+    public void daoPointcut(){}
 
-    @Before("controllerPointCut()")
-    public void controllersBeforeLogger(JoinPoint joinPoint){
+    @Before("daoPointcut()")
+    public void daoBeforeLogger(JoinPoint joinPoint){
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Object[] args = joinPoint.getArgs();
 
         log.info("[Before] "+signature+" -> arguments passed : "+ Arrays.toString(args));
     }
 
-    @AfterReturning(value = "controllerPointCut()", returning = "result")
-    public void controllersAfterLogger(JoinPoint joinPoint, Object result){
+    @AfterReturning(value = "daoPointcut()", returning = "result")
+    public void daoAfterLogger(JoinPoint joinPoint, Object result){
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
         log.info("[After Returning] "+signature+" -> returned : "+ result);
     }
 
-    @AfterThrowing(value = "controllerPointCut()", throwing = "excep")
-    public void controllersAfterLogger(JoinPoint joinPoint, Exception excep){
+    @AfterThrowing(value = "daoPointcut()", throwing = "excep")
+    public void daoAfterLogger(JoinPoint joinPoint, Exception excep){
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
 
         log.warning("[After Throwing] "+signature+" -> throws : "+ excep.getMessage());
